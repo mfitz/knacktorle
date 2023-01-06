7e371cbe75662cc80b7fc12591d4d24c27442cf6
Michael Fitzmaurice <Michael.Fitzmaurice@arup.com>
2023-01-06 00:57:25 +0000

    Fix the clues file transformer

--- actorle_file_transformer.py
+++ actorle_file_transformer.py
@@ -13,11 +13,12 @@ if __name__ == '__main__':
         print("The raw content must be on a single line - not sure how to handle this file, exiting")
         sys.exit(1)
     raw_content = raw_content[0]
-    preamble_html = '<table cellspacing="0" cellpadding="0"><thead><tr class="css-10vn1a1"><th align="left" class="css-tq6b8x"><div class="css-11d1x01"><button type="button" class="ant-btn ant-btn-circle ant-btn-primary ant-btn-sm"><span>?</span></button></div>Title</th><th align="left" class="css-be3ggk">Genres</th><th align="left" class="css-1liovd5">IMDB</th></tr></thead><tbody>'
+    preamble_html = '<table cellspacing="0" cellpadding="0"><thead><tr class="css-yww95x"><th align="left" class="css-yxjuuu"><div class="css-11d1x01"><button type="button" class="ant-btn ant-btn-circle ant-btn-primary ant-btn-sm"><span>?</span></button></div>Movie Title</th><th align="left" class="css-x4em2r">Genres</th><th align="left" class="css-hz967x">IMDB</th></tr></thead><tbody>'
     transformed_content = raw_content.replace(preamble_html, '')
-    transformed_content = transformed_content.replace('<tr><td class="css-1k81oxp"><div class="css-cp6cch"><span class="css-bopld4">', '\n')
-    transformed_content = transformed_content.replace('</span></div><div class="css-bopld4">', '|')
-    transformed_content = transformed_content.replace('</div></td><td class="css-acurbl">', '|')
+    transformed_content = transformed_content.replace('<tr><td class="css-78tlv6"><div class="css-16gxh5a"><span class="css-mmvz9h">', '\n')
+    transformed_content = transformed_content.replace('</span></div><div class="css-1fapvp5">', '|')
+    transformed_content = transformed_content.replace('</div><div class="css-iyxy5d"></div></td><td class="css-wen7vf">', '|')
+    transformed_content = transformed_content.replace('</span></td><td class="css-ptlbxk"><div class="css-zjik7"><div class="css-19ufyx7">', '|')
     transformed_content = transformed_content.replace('</div></div></td></tr>', '')
     transformed_content = re.sub('</span><span class="ant-tag css-[a-z0-9]{5,}">', ',', transformed_content)
     transformed_content = transformed_content.replace('</span></td><td class="css-1k81oxp"><div class="css-zjik7"><div class="css-1ntf0l5">', '|')
