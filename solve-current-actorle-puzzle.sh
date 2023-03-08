bf3770efe0a8aff1f606d861eaf86149397f498a
Michael Fitzmaurice <Michael.Fitzmaurice@arup.com>
2023-03-08 00:16:14 +0000

    Make the daily solver script support abritrary numbers of additional params

--- solve-current-actorle-puzzle.sh
+++ solve-current-actorle-puzzle.sh
@@ -1,3 +1,3 @@
 # !/bin/bash
 
-time python actorle_solver.py --movies-file data/title.basics.tsv.gz --performances-file data/title.principals.tsv.gz --actors-file data/name.basics.tsv.gz
+time python actorle_solver.py --movies-file data/title.basics.tsv.gz --performances-file data/title.principals.tsv.gz --actors-file data/name.basics.tsv.gz "$@"
