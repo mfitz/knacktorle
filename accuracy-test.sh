22f73a9a181368eb2c8d20d7f7c49997c4d681b2
Michael Fitzmaurice <Michael.Fitzmaurice@arup.com>
2023-12-24 21:33:37 +0000

    Add the accuracy test launch script

---
+++ accuracy-test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+pushd "${0%/*}" > /dev/null
+
+time PYTHONPATH=$PYTHONPATH:. \
+python tests/integration/accuracy-test.py \
+--puzzle-directory clues-files \
+--answers-file data/puzzle-actual-answers.csv \
+--solver-script $PWD/solve-current-actorle-puzzle.sh \
+"$@"
+
+popd > /dev/null
