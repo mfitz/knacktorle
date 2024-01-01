978922ff378c52325444d1a9e3c761c8bd8db26b
Michael Fitzmaurice <Michael.Fitzmaurice@arup.com>
2024-01-01 19:23:38 +0000

    Add more unit tests for the solver

--- tests/test_actorle_solver.py
+++ tests/test_actorle_solver.py
@@ -16,7 +16,25 @@ def test_matches_movie_with_exact_review_rating_match():
     titles_dataframe = pd.DataFrame(data=movie_data)
 
     matched_movies = actorle_solver.get_matching_movie_ids(titles_dataframe, clue, 0.0)
-    print(matched_movies.to_dict())
+
+    assert matched_movies.to_dict() == {
+        'primaryTitle': {0: 'Mad Max'},
+        'tconst': {0: 'tt0079501'}
+    }
+
+
+def test_matches_movie_with_review_rating_just_inside_tolerance():
+    clue = MovieClue('xxx xxx', '1979', 'Adventure,Action,Thriller,Science Fiction', 6.9)
+    movie_data = {
+        'tconst': ['tt0079501'],
+        'primaryTitle': ['Mad Max'],
+        'startYear': ['1979'],
+        'averageRating': [6.8]
+    }
+    titles_dataframe = pd.DataFrame(data=movie_data)
+
+    matched_movies = actorle_solver.get_matching_movie_ids(titles_dataframe, clue, 0.1)
+
     assert matched_movies.to_dict() == {
         'primaryTitle': {0: 'Mad Max'},
         'tconst': {0: 'tt0079501'}
